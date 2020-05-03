fb88b9e884272a90ed798cb4f545d1b2e51e4656
shubhamkaur <64738401+shubhamkaur@users.noreply.github.com>
2020-05-03 20:20:26 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,18 +18,17 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
+hey! {}, my name is {}!  
 
-im a group manager bot maintained by  [this person](tg://user?id={}).
+I can help you out in managing  your group efficiency . 
 
-My future updates will be put into This Channel - @MarieChechi & My Support Group @InFoTelGroup.
+[I am very smart that's because i am created by this guy !](tg://user?id={}).
 
-This is my [Deploy Code](https://heroku.com/deploy?template=https://github.com/TGExplore/Marie-2.0-English),
-you can create clone same like me..
+I am more powerful & I have more features than any other bots .
 
-For more commands click /help...
+Just Add me in group : Click "[HERE](http://t.me/chhotabheem_bot?startgroup=botstart)" . & Start Using .
 
-**Keep in mind that any changes you DO do to the source have to be on github, as per the license.**
+You can find my list of available commands with /help .
 
 """
 
